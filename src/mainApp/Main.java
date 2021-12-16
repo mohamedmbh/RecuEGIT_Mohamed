5d4d0629c73f3b8c3f39badc6171e7354cb45bc2
mohamedmbh <mohamedbenhamzaa@gmail.com>
2021-12-16 14:30:18 +0100

    Añade multiplicación y división

--- src/mainApp/Main.java
+++ src/mainApp/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		System.out.print("Escribe el segundo número: ");
 		int n2Input = Integer.parseInt(scanner.nextLine());
 		
-		System.out.println("1. Suma\n2. Resta");
+		System.out.println("1. Suma\n2. Resta\n3. Multiplicacion\n4. División");
 		System.out.print("Elige la operación que deseas: ");
 		int operacionInput = Integer.parseInt(scanner.nextLine());
 		
@@ -27,12 +27,25 @@ public class Main {
 		return n1 - n2;
 	}
 	
+	private static int multiplicacion(int n1, int n2) {
+		return n1 * n2;
+	}
+	
+	private static int division(int n1, int n2) {
+		if (n2 == 0) return 0;
+		return n1 / n2;
+	}
+	
 	public static int calcular (int n1, int n2, int operacion) {
 		switch (operacion) {
 		case 1:
 			return suma(n1, n2);
 		case 2:
 			return resta(n1, n2);
+		case 3:
+			return multiplicacion(n1, n2);
+		case 4:
+			return division(n1, n2);
 		default:
 			break;
 		}
